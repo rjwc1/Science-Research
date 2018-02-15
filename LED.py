d26eae4867a82621639e82899cfe6d6ad68ada48
RJ Wakefield-Carl <rjwc123@gmail.com>
2018-02-14 22:55:16 -0800

    added decode

--- LED.py
+++ LED.py
@@ -152,6 +152,69 @@ def receivemorse():
             list.append(DOT)
 
 
+encoding = {
+    'A': '.-',
+    'B': '-...',
+    'C': '-.-.',
+    'D': '-..',
+    'E': '.',
+    'F': '..-.',
+    'G': '--.',
+    'H': '....',
+    'I': '..',
+    'J': '.---',
+    'K': '-.-',
+    'L': '.-..',
+    'M': '--',
+    'N': '-.',
+    'O': '---',
+    'P': '.--.',
+    'Q': '--.-',
+    'R': '.-.',
+    'S': '...',
+    'T': '-',
+    'U': '..-',
+    'V': '...-',
+    'W': '.--',
+    'X': '-..-',
+    'Y': '-.--',
+    'Z': '--..',
+    # Seen in use on the uncyclopedia:
+    "'": '.----.'}
+decoding = {}
+for key, val in encoding.items():
+    decoding[val] = key
+
+
+def encode(text):
+    # should really pre-process {'.': 'stop', ',': 'comma', '-': 'dash', ...}
+    return ' '.join(map(lambda x, g=encoding.get: g(x, ' '), text.upper()))
+
+
+def decode(message):
+    ans = ''.join(map(lambda x, g=decoding.get: g(x, ' '), message.split(' ')))
+    return ' '.join(ans.split())  # tidy up spacing
+
+
+def decipher(message):
+    # like decode, but when there are no spaces.
+    row = [('', message)]
+    while filter(lambda x: x[1], row):
+        old = row
+        row = []
+        for it in old:
+            txt, code = it
+            if code:
+                for (t, c) in encoding.items():
+                    if code[:len(c)] == c:
+                        row.append((txt + t, code[len(c):]))
+                # NB we discard it if no initial segment of code matches an encoding.
+            else:
+                row.append(it)
+
+    return map(lambda it: it[0], row)
+
+
 setup()
 receivemorse()
 print(list[:])
